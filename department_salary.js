da1eab1a538c9cef01b79b39cfabd4844f58fcf5
Evelyn_ Hernandez <eh22@usf.edu>
2024-10-06 20:43:49 -0400

    Initialize company structure with departments and employees

---
+++ department_salary.js
@@ -0,0 +1,103 @@
+// Task 1: Create a Department Structure
+const company = {
+
+        departments: [
+    
+            {
+    
+                departmentName: 'Marketing',
+    
+               employees: [
+    
+                   {
+    
+                       name: 'Paige',
+                      salary: 100000,
+    
+                        subordinates: [
+    
+                            {
+    
+                                name: 'Chris',
+    
+                              salary: 80000,
+    
+                                subordinates: [
+    
+                                   {
+    
+                                        name: 'Mike',
+    
+                                       salary: 60000,
+    
+                                       subordinates: []
+    
+                                   }
+    
+                               ]
+    
+                           }
+    
+                       ]
+    
+                  },
+    
+                    {
+    
+                       name: 'Sam',
+    
+                        salary: 90000,
+    
+                       subordinates: []
+    
+                   }
+    
+               ]
+    
+            },
+    
+            {
+    
+              departmentName: 'Sales',
+    
+               employees: [
+    
+                   {
+    
+                       name: 'Katie',
+    
+                       salary: 85000,
+    
+                       subordinates: [
+    
+                           {
+    
+                               name: 'Jonas',
+    
+                               salary: 70000,
+    
+                                subordinates: []
+    
+                          }
+    
+                       ]
+    
+                   },
+    
+                   {
+    
+                      name: 'Faith',
+    
+                       salary: 95000,
+    
+                      subordinates: []
+    
+                  }
+    
+               ]
+    
+           }
+    
+       ]
+    
+    };
