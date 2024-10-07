8c97cd8b78c8c41e478ce3976bb16dd3d27d9670
Evelyn_ Hernandez <eh22@usf.edu>
2024-10-06 21:47:58 -0400

    Create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -51,3 +51,32 @@ const company = {
         }
     ]
 };
+// Task 2: Create a Recursive Function to Calculate Total Salary for a Department
+function calculateDepartmentSalary (department) {
+    let totalSalary = 0;
+
+department.employees.forEach(function (employee){ // loops through employees
+    totalSalary += employee.salary;
+
+    employee.subordinates.forEach(function(subordinate){ // checking for subordinates and salaries
+        totalSalary += subordinate.salary;
+
+        totalSalary += calculateEmployeeSalary(subordinate); // if subordinate has employees then recursive
+    });
+});
+console.log (`Total Salary for ${department.departmentName} : $${totalSalary}`); // pulls up marketing department salary
+return totalSalary;
+}
+
+// checking salary of employee and subordinates
+function calculateEmployeeSalary (employee) {
+    let employeeTotal = employee.salary;
+    employee.subordinates.forEach(function(subordinate){
+        employeeTotal += calculateEmployeeSalary(subordinate);
+    });
+    return employeeTotal;
+}
+
+//testing for a department
+const totalSalesSalary = calculateDepartmentSalary(company.departments [0]);
+console.log (`Total Salary for the Sales Department: $${totalSalesSalary}`); // pulls up sales department salary
